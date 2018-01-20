648240c94bf1cb1d5b9d1448aed0a85783c56508
root <root@cyber-desktop.localdomain>
2018-01-20 01:00:53 +0100

    Added first version of installation script

---
+++ suse-x11-install.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+#
+#    Installation script for installing X11 environment on WSL
+#    ---------------------------------------------------------
+#
+#    by Markus Guertler
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#
+
+echo ""
+echo "--- Updating system ---"
+echo ""
+zypper ref; zypper -y up
+
+echo ""
+echo "--- Installing patterns & packages ---"
+echo ""
+zypper install -y -t pattern yast2_basis x11 xfce
+
+echo ""
+echo "--- Configuring display forwarding ---"
+echo ""
+
+USERS=$(ls -d /home/*)
+USERS="$USERS /root"
+
+for USER in $USERS; do
+	if grep -Fxq "export DISPLAY=\":0" $USER/.bashrc
+	then
+		echo "Info: Display forwarding already setup for $USER"
+	else
+		echo "export DISPLAY=\":0" >>$USER/.bashrc
+	fi
+done
+
+echo ""
+echo "--- Fixing d-bus issue ---"
+echo ""
+
+sudo mkdir /var/run/dbus
+sudo sed -i 's$<listen>.*</listen>$<listen>tcp:host=localhost,port=0</listen>$' /etc/dbus-1/session.conf
+
+echo ""
+echo "--- Downloading vcXsrv ---"
+echo ""
+CMD="/mnt/c/Windows/System32/cmd.exe"
+LATEST="vcxsrv-latest.exe"
+wget -O /tmp/$LATEST https://sourceforge.net/projects/vcxsrv/files/latest/download
+mv /tmp/$LATEST /mnt/c/Windows/Temp
+
+echo ""
+echo "!!!                                                  !!!"
+echo "!!! The installation script would now install vcXsrv !!!"
+echo "!!!              Press any key to continue           !!!"
+echo "!!!                                                  !!!"
+echo ""
+
+read -n 1 -s -r
+
+echo ""
+echo "--- Installing vcXsrv ---"
+echo ""
+$CMD /c "c:/Windows/Temp/$LATEST /S"
